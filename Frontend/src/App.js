fef8554c6e5dac5ed3fb9ea7e57a5a1ea346d75b
DIVYA YELMAKANNE <divyayelmakanne@gmail.com>
2025-07-04 11:32:21 +0530

    Add files via upload

---
+++ Frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import VoiceBridgeDemo from "./components/VoiceBridgeDemo";
+
+function App() {
+  return (
+    <div>
+      <VoiceBridgeDemo />
+    </div>
+  );
+}
+
+export default App; 
